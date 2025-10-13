bb0995c896821230741d14fbd97e54f32dd77965
Guilherme Storch de Oliveira <gsoliveira@brasal.com.br>
2025-10-12 21:54:06 -0300

    Implementa heap e cálculo guloso

--- 857.cpp
+++ 857.cpp
@@ -1,13 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
         int n = quality.size();
-        vector<pair<double, int>> workers; // (ratio, quality)
-        for (int i = 0; i < n; ++i) {
-            double ratio = (double)wage[i] / quality[i];
-            workers.push_back({ratio, quality[i]});
+        vector<pair<double, int>> workers;
+        for (int i = 0; i < n; ++i)
+            workers.push_back({(double)wage[i] / quality[i], quality[i]});
+        sort(workers.begin(), workers.end());
+
+        priority_queue<int> maxHeap; // guarda as maiores qualidades
+        long long sumQ = 0;
+        double minCost = numeric_limits<double>::infinity();
+
+        for (auto& [ratio, q] : workers) {
+            maxHeap.push(q);
+            sumQ += q;
+            if ((int)maxHeap.size() > k) {
+                sumQ -= maxHeap.top();
+                maxHeap.pop();
+            }
+            if ((int)maxHeap.size() == k) {
+                minCost = min(minCost, ratio * sumQ);
+            }
         }
-        // TODO: ordenar por ratio e montar lógica de seleção
-        return 0.0;
+        return minCost;
     }
 };
