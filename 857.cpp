b32c8d9f2a9ffc82d0770a6adf104606b3ae600d
Guilherme Storch de Oliveira <gsoliveira@brasal.com.br>
2025-10-12 22:16:36 -0300

    polimento final

--- 857.cpp
+++ 857.cpp
@@ -3,28 +3,32 @@ using namespace std;
 
 class Solution {
 public:
-    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+    double mincostToHireWorkers(const vector<int>& quality, const vector<int>& wage, int k) {
         int n = quality.size();
         vector<pair<double, int>> workers;
+        workers.reserve(n);
         for (int i = 0; i < n; ++i)
-            workers.push_back({(double)wage[i] / quality[i], quality[i]});
+            workers.emplace_back((double)wage[i] / quality[i], quality[i]);
+
         sort(workers.begin(), workers.end());
 
-        priority_queue<int> maxHeap; // guarda as maiores qualidades
+        priority_queue<int> topQ;   // guarda as maiores qualidades
         long long sumQ = 0;
-        double minCost = numeric_limits<double>::infinity();
+        double ans = numeric_limits<double>::infinity();
 
         for (auto& [ratio, q] : workers) {
-            maxHeap.push(q);
+            topQ.push(q);
             sumQ += q;
-            if ((int)maxHeap.size() > k) {
-                sumQ -= maxHeap.top();
-                maxHeap.pop();
+
+            if ((int)topQ.size() > k) {
+                sumQ -= topQ.top();
+                topQ.pop();
             }
-            if ((int)maxHeap.size() == k) {
-                minCost = min(minCost, ratio * sumQ);
+
+            if ((int)topQ.size() == k) {
+                ans = min(ans, ratio * (double)sumQ);
             }
         }
-        return minCost;
+        return ans;
     }
 };
